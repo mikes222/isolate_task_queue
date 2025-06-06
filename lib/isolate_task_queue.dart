86ff17544b32282712caf093fc5600c40be82da2
mikes222 <bitbucket@mschwartz.eu>
2025-06-06 10:05:27 +0200

    Introducing new code

---
+++ lib/isolate_task_queue.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/flutter_isolate.dart';
+export 'src/queue_cancelled_exception.dart';
+export 'src/task_queue.dart';
